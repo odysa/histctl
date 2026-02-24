e949b6018a58413033af7d0b043f5c253b64d841
Chengxu Bian <cbian564@gmail.com>
2026-02-23 20:16:25 -0500

    Merge all browser paths into single file with runtime.GOOS
    Replace paths_{darwin,linux,windows}.go with one paths.go using a
    runtime.GOOS switch. Only paths_darwin.go remains for Safari registration.

--- internal/browser/paths_darwin.go
+++ internal/browser/paths_darwin.go
@@ -2,20 +2,7 @@
 
 package browser
 
-import "path/filepath"
-
-var knownBrowsers = []string{"safari", "chrome", "edge", "firefox"}
-
-var chromeDBSubPath = filepath.Join("Library", "Application Support", "Google", "Chrome", "Default", "History")
-var edgeDBSubPath = filepath.Join("Library", "Application Support", "Microsoft Edge", "Default", "History")
-var firefoxProfileBase = filepath.Join("Library", "Application Support", "Firefox", "Profiles")
-
-const (
-	chromeProcessName  = "Google Chrome"
-	edgeProcessName    = "Microsoft Edge"
-	firefoxProcessName = "firefox"
-)
-
 func init() {
+	knownBrowsers = append([]string{"safari"}, knownBrowsers...)
 	constructors["safari"] = func() Browser { return NewSafari("") }
 }
